59c79334288c3ff023cfea18b21d3a64213c66f4
vplameniraket <danscreativevision@gmail.com>
2019-04-20 15:07:13 +0300

    Update

--- Duolingo-Advanced-Learning.js
+++ Duolingo-Advanced-Learning.js
@@ -14,41 +14,44 @@ setInterval(function(){
 		if (!!textarea) {
 			if (!isFound) {
 				isFound = true;
+
 				newid = Math.floor(Math.random() * Math.floor(1000));
 				textarea.id = 'textarea' + newid;
+
 				var frames = document.querySelectorAll('._1eYrt._3T--_, ._1ZoK4._3T--_');
 				frames[frames.length - 1].id = 'frame' + newid;
 				parent = document.getElementById('frame' + newid);
+
 				var header = parent.querySelectorAll('h1._1Zqmf[data-test="challenge-header"] span')[0];
 				if (header.innerText == 'Write this in English') header.innerText = 'Write down and translate what you hear';
+
 				textarea.parentNode.insertAdjacentHTML('beforebegin', '<div id="newwrap' + newid + '" class="newwrap animated fast"><textarea data-test="challenge-translate-input" id="typewhatyouhear' + newid + '" class="_7q434 _1qCW5 _2fPEB _3_NyK _1Juqt typewhatyouhear" dir="ltr" placeholder="Type what you hear"></textarea><div class="badge">1</div><audio id="successSound' + newid + '"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571470/DuolingoSuccessMP3.mp3" type="audio/mpeg"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571639/DuolingoSuccessOGG.ogg" type="audio/ogg"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571640/DuolingoSuccessWAV.wav" type="audio/wav"></audio></div>');
-				textarea.insertAdjacentHTML('afterend','<div class="badge">2</div>');
 				textarea.parentNode.classList.add('relative');
+				textarea.insertAdjacentHTML('afterend','<div class="badge">2</div>');
+				textarea.classList.add('secondary');
+				textarea.disabled = true;
+				
 				var newarea = document.getElementById('typewhatyouhear' + newid);
 				document.getElementById('newwrap' + newid).parentNode.classList.add('flexrow');
 				newarea.classList.add('green');
 				newarea.focus();
-				textarea.disabled = true;
-				textarea.classList.add('secondary');
+
 
 				var elements = parent.querySelectorAll('div[data-test="challenge-translate-prompt"]'),
 					sentence = "";
 				elements = elements[elements.length - 1].querySelectorAll('[data-test="hint-token"]');
-				for (var i = 0; i < elements.length; i++) {
-					sentence += elements[i].innerText;
-				}
-				sentence = sentence.toLowerCase();
+				for (var i = 0; i < elements.length; i++) sentence += elements[i].innerText.toLowerCase();
 
-				var nativeKeyboard = parent.querySelectorAll('#textarea' + newid + '' + textareaSel + ' + .I1fg4')[0];
+				var nativeKeyboardButtons = parent.querySelectorAll('#textarea' + newid + ' + .I1fg4 ._1tSEs.oNqWF._3hso2._3skMI._1AM95');
 				function toggleNativeKeyboard(state) {
-					if (!!nativeKeyboard) {
-						var nativeKeyboardButtons = parent.querySelectorAll('#textarea' + newid + '' + textareaSel + ' + .I1fg4 ._1tSEs.oNqWF._3hso2._3skMI._1AM95');
+					if (!!parent.querySelectorAll('#textarea' + newid + ' + .I1fg4')[0]) {
 						for (var i = 0; i < nativeKeyboardButtons.length; i++) {
 							nativeKeyboardButtons[i].disabled = state;
 						}
 					}
 				}
-				var keybuttons = parent.querySelectorAll('._1tSEs.oNqWF._3hso2._3skMI._1AM95');
+
+				var keybuttons = parent.querySelectorAll('#newwrap' + newid + ' ._1tSEs.oNqWF._3hso2._3skMI._1AM95');
 				function toggleNewKeyboard(state) {
 					if (!!parent.querySelectorAll('#newwrap' + newid + ' .I1fg4')[0]) {
 						for (var i = 0; i < keybuttons.length; i++) {
@@ -56,6 +59,7 @@ setInterval(function(){
 						}
 					}
 				}
+
 				toggleNativeKeyboard(true);
 				newarea.addEventListener("keydown", function (e) {
 					if (e.keyCode === 13) validate(e);
@@ -63,8 +67,10 @@ setInterval(function(){
 						textarea.disabled = false;
 						textarea.classList.replace('secondary', 'green');
 						textarea.focus();
+
 						newarea.disabled = true;
 						newarea.classList.add('secondary');
+
 						toggleNewKeyboard(true);
 						toggleNativeKeyboard(false);
 					}
@@ -82,15 +88,19 @@ setInterval(function(){
 						textarea.classList.replace('secondary', 'green');
 						textarea.disabled = false;
 						textarea.focus();
+
 						newarea.disabled = true;
+						newarea.classList.add('secondary','success');
+
 						newwrap.classList.replace('fast','faster');
 						newwrap.classList.add('pulse');
-						newarea.classList.add('secondary','success');
+						
 						var player = document.getElementById('successSound' + newid);
-						toggleNewKeyboard(true);
-						toggleNativeKeyboard(false);
 						player.volume = 0.3;
 						player.play();
+
+						toggleNewKeyboard(true);
+						toggleNativeKeyboard(false);
 					}
 					else {
 						newwrap.classList.remove('shake');
@@ -102,7 +112,7 @@ setInterval(function(){
 				}
 				if (keyboardDefined && getItm('keyboard') !== '') {
 					document.getElementById('newwrap' + newid).insertAdjacentHTML('beforeend', '<div class="I1fg4 _3bmJ2">' + getItm('keyboard') + '</div>');
-					var keybuttons = parent.querySelectorAll('._1tSEs.oNqWF._3hso2._3skMI._1AM95'),
+					var keybuttons = parent.querySelectorAll('#newwrap' + newid + ' ._1tSEs.oNqWF._3hso2._3skMI._1AM95');
 						keybuttonslength = keybuttons.length;
 
 					function lowerCase() {
@@ -117,18 +127,10 @@ setInterval(function(){
 							keybuttons[i].innerText = keybuttons[i].innerText.toUpperCase();
 						}
 					}
-					function enableButtons() {
-						for (var i = 0; i < keybuttonslength; i++) {
-							keybuttons[i].disabled = false;
-						}
-					}
-					function resetRegister(){
-						if (keybuttons[0].innerText == '↓') {
-							lowerCase();
-						}
-					}
-					resetRegister();
-					enableButtons();
+
+					if (keybuttons[0].innerText == '↓') lowerCase();
+					for (var i = 0; i < keybuttonslength; i++) keybuttons[i].disabled = false;
+
 					keybuttons[0].onclick = function(){
 						if (this.innerText == '↓') lowerCase();
 						else upperCase();
@@ -143,7 +145,8 @@ setInterval(function(){
 			}
 		}
 	}
-	else if (typeof document.querySelectorAll('._1eYrt._3T--_ #textarea' + newid + '' + textareaSel + ', ._1ZoK4._3T--_  #textarea' + newid + '' + textareaSel)[0] === 'undefined') isFound = false;
+	else if (typeof document.querySelectorAll('._1eYrt._3T--_ #textarea' + newid + ', ._1ZoK4._3T--_  #textarea' + newid)[0] === 'undefined') isFound = false;
+
 	var targetLangInput = document.querySelectorAll('textarea[autocorrect][spellcheck]')[0];
 	if (!!targetLangInput){
 		var keyboard = document.querySelectorAll('textarea[autocorrect][spellcheck] + .I1fg4')[0];
