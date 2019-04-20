f8ec25976e6186e5c581cd02346e98661796027d
vplameniraket <danscreativevision@gmail.com>
2019-04-20 10:56:31 +0300

    Update

--- Duolingo-Advanced-Learning.js
+++ Duolingo-Advanced-Learning.js
@@ -4,26 +4,21 @@ function getItm(prop) { return localStorage.getItem(prop); }
 var keyboardDefined = false;
 if (getItm('keyboard')) keyboardDefined = true;
 
-function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
-}
 var isFound = false,
-	idDefined = false,
 	newid,
 	newArea,
 	parent;
 setInterval(function(){
-	if (!isFound && !idDefined) {
+	if (!isFound) {
 		var textarea = document.querySelectorAll('._1eYrt._3T--_ textarea[data-test="challenge-translate-input"]:not([autocorrect]):not([spellcheck]), ._1ZoK4._3T--_  textarea[data-test="challenge-translate-input"]:not([autocorrect]):not([spellcheck])')[0];
 		if (!!textarea) {
 			if (!isFound) {
 				isFound = true;
-				newid = getRandomInt(1000);
+				newid = Math.floor(Math.random() * Math.floor(1000));
 				textarea.id = 'textarea' + newid;
 				var frames = document.querySelectorAll('._1eYrt._3T--_, ._1ZoK4._3T--_');
 				frames[frames.length - 1].id = 'frame' + newid;
 				parent = document.getElementById('frame' + newid);
-				idDefined = true;
 				textarea.parentNode.insertAdjacentHTML('beforebegin', '<div id="newwrap' + newid + '" class="newwrap animated fast"><textarea data-test="challenge-translate-input" id="typewhatyouhear' + newid + '" class="_7q434 _1qCW5 _2fPEB _3_NyK _1Juqt typewhatyouhear" dir="ltr" placeholder="Type what you hear"></textarea><div class="badge">1</div><audio id="successSound' + newid + '"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571470/DuolingoSuccessMP3.mp3" type="audio/mpeg"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571639/DuolingoSuccessOGG.ogg" type="audio/ogg"><source src="https://res.cloudinary.com/dn6n8yqqh/video/upload/v1555571640/DuolingoSuccessWAV.wav" type="audio/wav"></audio></div>');
 				textarea.insertAdjacentHTML('afterend','<div class="badge">2</div>');
 				textarea.parentNode.classList.add('relative');
@@ -70,9 +65,12 @@ setInterval(function(){
 				}
 				function validate(e) {
 					var text = e.target.value.replace(/[ ]$/g, '').replace(/^[ ]/g, '').replace(/[/:]/g, ''),
-						filteredSentence = sentence.replace(/[ ][.?!]$/g, '').replace(/[.?!]$/g, '').replace(/[¿]/g, ''),
+						filteredSentence = sentence.replace(/[ ][.?!]$/g, '').replace(/[.?!]$/g, '').replace(/[¿]/g, '').replace(/[¡]/g, ''),
 						newwrap = document.getElementById('newwrap' + newid);
-					if (text.toLowerCase() == sentence.toLowerCase() || text.toLowerCase() == filteredSentence.toLowerCase() || text.toLowerCase() == sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() || text.toLowerCase() == filteredSentence.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()) {
+					if (text.toLowerCase() == sentence.toLowerCase() ||
+						text.toLowerCase() == filteredSentence.toLowerCase() ||
+						text.toLowerCase() == sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() ||
+						text.toLowerCase() == filteredSentence.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()) {
 						textarea.disabled = false;
 						textarea.classList.remove('secondary');
 						textarea.classList.add('green');
@@ -157,7 +155,6 @@ setInterval(function(){
 		newArea = document.querySelectorAll('._1eYrt._3T--_ #textarea' + newid + '[data-test="challenge-translate-input"]:not([autocorrect]):not([spellcheck]), ._1ZoK4._3T--_  #textarea' + newid + '[data-test="challenge-translate-input"]:not([autocorrect]):not([spellcheck])')[0];
 		if (typeof newArea === 'undefined') {
 			isFound = false;
-			idDefined = false;
 		}
 	}
 	var targetLangInput = document.querySelectorAll('textarea[autocorrect][spellcheck]')[0];
