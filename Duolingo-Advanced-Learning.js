30d40cf040a03a368a050e5a30385a6752c68e84
vplameniraket <danscreativevision@gmail.com>
2019-04-22 18:13:16 +0300

    Update

--- Duolingo-Advanced-Learning.js
+++ Duolingo-Advanced-Learning.js
@@ -77,7 +77,7 @@ setInterval(function(){
 				});
 				function validate(e) {
 					function removeDotAndExcMarks(string) { return string.replace(/[ ][.?!]$/g, '').replace(/[.?!]$/g, '').replace(/[¿]/g, '').replace(/[¡]/g, ''); }
-					function replaceNonLatin(string) { return string.replace(/[æ]/g, 'ae').replace(/[ø]/g, 'oe').replace(/[å]/g, 'aa').replace(/[ß]/g, 'ss').replace(/[œ]/g, 'oe').replace(/[ñ]/g, 'n\''); }
+					function replaceNonLatin(string) { return string.replace(/[æ]/g, 'ae').replace(/[ø]/g, 'oe').replace(/[å]/g, 'aa').replace(/[ß]/g, 'ss').replace(/[œ]/g, 'oe').replace(/[ñ]/g, 'n\'').replace(/[ǆ]/g, 'dzh'); }
 					function normalize(string) { return string.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); }
 					var text = e.target.value.replace(/[ ]$/g, '').replace(/^[ ]/g, '').replace(/[/:]/g, '').replace(/[ ][ ]/g, ' ').toLowerCase(),
 						noCommas = sentence.replace(/[,][ ]/g, ' '),
